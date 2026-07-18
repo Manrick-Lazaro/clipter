b2a0ddbb657e3d38b10745ee1a14f2409302fa13
Manrick Silva <manrick.l.silva@gmail.com>
2026-07-18 11:41:10 -0300

    cria menu principal

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from PySide6.QtWidgets import QApplication
+
+from windows.main_window import MainWindow
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
