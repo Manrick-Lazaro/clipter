ba771f39020df8ebdc0873f1947e1f84069c8287
Manrick Silva <manrick.l.silva@gmail.com>
2026-07-20 20:37:24 -0300

    adiciona sistema de navegação

--- windows/main_window.py
+++ windows/main_window.py
@@ -1,29 +1,58 @@
-from PySide6.QtWidgets import QMainWindow, QStackedWidget
+from PySide6.QtWidgets import (
+    QMainWindow, 
+    QStackedWidget,
+    QWidget,
+    QHBoxLayout,
+)
 
 from pages.download_page import DownloadPage
 from pages.transcription_page import TranscriptionPage
+from widgets.side_menu import SideMenu
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle("Clipter")
+        self.resize(1024, 768)
+
         # WIDGETS
+        self.menu = SideMenu()
         self.stack = QStackedWidget()
-        
-        self.download_page = DownloadPage(self)
-        self.transcription_page = TranscriptionPage(self)
+        self.download_page = DownloadPage()
+        self.transcription_page = TranscriptionPage()
 
         # STACKS PAGES
         self.stack.addWidget(self.download_page)
         self.stack.addWidget(self.transcription_page)
 
-        self.setCentralWidget(self.stack)
+        # LAYOUT
+        central = QWidget()
+        
+        layout = QHBoxLayout(central)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
 
-        # INICIAL PAGE
-        self.show_download_page()
+        layout.addWidget(self.menu)
+        layout.addWidget(self.stack)
+        
+        self.setCentralWidget(central)
 
+        self.menu.bt_download.clicked.connect(
+            self.show_download_page
+        )
+
+        self.menu.bt_transcription.clicked.connect(
+            self.show_transcription_page
+        )
     def show_download_page(self):
         self.stack.setCurrentWidget(self.download_page)
-    
+        self.menu.set_active_button(
+            self.menu.bt_download
+        )
+
     def show_transcription_page(self):
         self.stack.setCurrentWidget(self.transcription_page)
+        self.menu.set_active_button(
+            self.menu.bt_transcription
+        )
